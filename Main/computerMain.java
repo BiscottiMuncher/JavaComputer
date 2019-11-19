eb34e9ba4e18819efe8162803418378b752b95ea
BiscottiMuncher <55550077+BiscottiMuncher@users.noreply.github.com>
2019-11-18 22:19:31 -0800

    Create computerMain.java

---
+++ Main/computerMain.java
@@ -0,0 +1,9 @@
+public class computerMain {
+    public static void main(String[] args) {
+        CPU AmdPhenom = new CPU("Phenom X4 9750", 35.0, 2400, 4, 4, 512, 95);
+        System.out.println(AmdPhenom);
+
+
+
+    }
+}
